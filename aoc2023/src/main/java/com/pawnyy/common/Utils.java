26241923a125b2f0e9b5f5e4d4fa4d4cdd40f87f
Pawnyy <sfaronoff@gmail.com>
2023-12-01 17:16:24 +0000

    day01 part 1 complete

--- aoc2023/src/main/java/com/pawnyy/common/Utils.java
+++ aoc2023/src/main/java/com/pawnyy/common/Utils.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.nio.Buffer;
 import java.util.function.Function;
 import java.io.IOException;
 
 public class Utils {
-    public static String readFileInput(String path) throws FileNotFoundException {
+    public static String readFileInput(String path) throws IOException {
         File file = new File(path);
         // Return the entire content of the file as a String
         BufferedReader br = new BufferedReader(new FileReader(file));
@@ -23,6 +22,7 @@ public class Utils {
         } catch (Exception e) {
             System.out.println(e);
         }
+        br.close();
         return result;
     }
 
--- aoc2023/src/main/java/com/pawnyy/day01/Answer.java
+++ aoc2023/src/main/java/com/pawnyy/day01/Answer.java
@@ -1,26 +1,30 @@
 package com.pawnyy.day01;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.pawnyy.common.*;
 
 public class Answer {
-    int sum = 0;
-    public static void main(String[] args) {
+    static int sum = 0;
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         part1();
+        System.out.println(sum);
     }
 
-    public static void part1() {
-
+    public static void part1() throws FileNotFoundException, IOException {
+        Utils.forEachLine("src/main/java/com/pawnyy/day01/input.txt", Answer::part1PerLine);
     }
 
-    public static void part1PerLine(String line) {
+    public static Void part1PerLine(String line) {
         ArrayList<Integer> nums = new ArrayList<>();
         for (char c : line.toCharArray()) {
             if (Character.isDigit(c)) {
                 nums.add(Character.getNumericValue(c));
             }
         }
-        sum += (nums.get(0) + nums.get(nums))
+        sum += ((nums.get(0)*10) + nums.get(nums.size() - 1));
+        return null;
     }
 }
