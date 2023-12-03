508ab5b936e28f5a6d3261762672e4957d3c9738
Pawnyy <sfaronoff@gmail.com>
2023-12-03 00:43:07 +0000

    day 2 part 2 complete

--- aoc2023/src/main/java/com/pawnyy/day02/Answer.java
+++ aoc2023/src/main/java/com/pawnyy/day02/Answer.java
@@ -11,8 +11,8 @@ import com.pawnyy.common.*;
 public class Answer {
     static int sum = 0;
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        part1();
-        // part2();
+        // part1();
+        part2();
         System.out.println(sum);
     }
 
@@ -66,5 +66,46 @@ public class Answer {
         return null;
     }
 
-    public static void part2() {}
+    public static void part2() throws FileNotFoundException, IOException {
+        Utils.forEachLine("src/main/java/com/pawnyy/day02/input.txt", Answer::part2PerLine);
+    }
+
+    public static Void part2PerLine(String line) {
+        final Pattern pattern = Pattern.compile("Game (\\d+): (.+)");
+        final Matcher matcher = pattern.matcher(line);
+        matcher.find();
+        int id = Integer.parseInt(matcher.group(1));
+        String gameStr = matcher.group(2);
+
+        int valid = 0;
+        int games = 0;
+
+        String[] perRoundArr = gameStr.split(";");
+        games = perRoundArr.length;
+        HashMap<String, Integer> colorsHashMap = new HashMap<String, Integer>();
+            
+        colorsHashMap.put("red", 0);
+        colorsHashMap.put("green", 0);
+        colorsHashMap.put("blue", 0);
+
+        for (String round : perRoundArr) {
+            String[] perDrawArr = round.split(",");
+            
+            for (String draw : perDrawArr) {
+                draw = draw.trim();
+                
+                String[] colorCount = draw.split(" ");
+                if (colorsHashMap.get(colorCount[1]) < Integer.parseInt(colorCount[0])) {
+                    colorsHashMap.put(colorCount[1], Integer.parseInt(colorCount[0]));
+                }
+                
+            }
+            
+            
+        }
+        int power = colorsHashMap.get("red") * colorsHashMap.get("green") * colorsHashMap.get("blue");
+        System.out.println("" + id + " has power " + power);
+        sum += power;
+        return null;
+    }
 }
